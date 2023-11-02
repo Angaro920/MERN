801648b32bc395794fd54da6497a97c65cf3db48
Angaro920 <95058616+Angaro920@users.noreply.github.com>
2023-11-02 18:31:10 -0500

    Update ActualizarUsuario.js

--- client/src/ActualizarUsuario.js
+++ client/src/ActualizarUsuario.js
@@ -7,33 +7,66 @@ function ActualizarUsuario() {
   };
   const [phonebook, setPhonebook] = useState([]);
   const [phone, setNewPhone] = useState(0);
-    useEffect(() => {
-      Axios.get("http://localhost:8080/get-phone").then((res) => {
-        setPhonebook(res.data.data.phoneNumbers);
-      });
-    }, []);
+  const [email, setNewEmail] = useState("");
+  useEffect(() => {
+    Axios.get("http://localhost:8080/get-phone").then((res) => {
+      setPhonebook(res.data.data.phoneNumbers);
+    });
+  }, []);
 
-  const updatePhone = (id) =>{
-       Axios.put('http://localhost:8080/update-phone/',{ id,phone })
-  }
+  const updatePhone = (id) => {
+    Axios.put("http://localhost:8080/update-phone/", { id, phone, email }).then(alert("Usuario Actualizado")).then(window.location.reload());
+  };
 
   return (
-    
     <div className="container">
-      {
-        phonebook.map((val,key) => {
-          return <div key={key} className="phone" >
-            <h1>{val.name}</h1>
-            <p>{val.phone}</p>
-            <input type="number" placeholder='update Phone...' onChange={(e) => {
-              setNewPhone(e.target.value)
-            }}/>
-            <button className="update-btn"  onClick={() => {updatePhone(val._id)}}>Update</button>
-          </div>
-        })
-      }
-    </div>  
-  )
+      <h1>Lista de Contactos</h1>
+      <div className="album">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 "></div>
+        {phonebook.map((val, key) => {
+          return (
+            <div key={key} className="phone">
+              <p></p>
+
+              <div className="card" style={estilo}>
+                <div className="card-body">
+                  <h5 className="card-title">{val.name}</h5>
+                  <p className="card-text">
+                    {val.phone}
+                    <p>{val.email}</p>
+                    <input
+                      type="number"
+                      className="form-control"
+                      placeholder="Actualizar Numero"
+                      onChange={(e) => {
+                        setNewPhone(e.target.value);
+                      }}
+                    />
+                    <input
+                      type="text"
+                      className="form-control mt-1"
+                      placeholder="Actualizar Correo"
+                      onChange={(e) => {
+                        setNewEmail(e.target.value);
+                      }}
+                    />
+                    <button
+                      className="btn btn-warning mt-1"
+                      onClick={() => {
+                        updatePhone(val._id);
+                      }}
+                    >
+                      Update
+                    </button>
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default ActualizarUsuario;
